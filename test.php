28b0b8e5541bd07ae83147f5849878a9493fe18e
Steveee33 <62248560+Steveee33@users.noreply.github.com>
2020-03-16 14:53:06 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+ <?php
+  
+ phpinfo(); //vypíše všechny informace
+ 
+ phpinfo(INFO_LICENSE); //vypíše informaci o licenci
+ 
+ phpinfo(INFO_LICENSE | INFO_CREDITS); //vypíše informaci o licenci a zásluhách 
+ 
+ 
+ ?>
