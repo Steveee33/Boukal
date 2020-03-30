ae1eaf10f8d9aaf419003fa238c580964fb2d720
Steveee33 <62248560+Steveee33@users.noreply.github.com>
2020-03-30 18:30:49 +0200

    Update oop.php

--- oop.php
+++ oop.php
@@ -1,10 +1,30 @@
  <?php
+//moc to nechápu, tak odevzdávám alespoň něco než nic. 
+//Štěpán Boukal ZT3
+class A {
+ publicfunction __construct($ustrednyPrefix.php) {    
+ }
 
+}
+class B {
+ publicfunction __construct($ustrednaPrefix.php) {    
+ }
+
+}
+class ustrednaPrefix extends ustrednyPrefix {}
+
+$napeti['ustrednaprefix'];
+ustrednaPrefix['setNapetiPrefix']="cislo";
 
 include('ustrednyPrefix.php');
+
 class ustrednyPrefix {Boukal};
-$napetiPrefix[´firstname´]=;
-$napeti ustrednaPrefix;
+
+$napetiPrefix['firstname']=Štěpán;
+
+$this=>$napeti = ustrednaPrefix;
+
+var_dump(ustrednaPrefix);
  
 
 
