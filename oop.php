d324c609f7b14c22648ea72fca3f52d5f0af9774
Steveee33 <62248560+Steveee33@users.noreply.github.com>
2020-03-30 23:39:53 +0200

    Update oop.php

--- oop.php
+++ oop.php
@@ -25,8 +25,63 @@ $napetiPrefix['firstname']=Štěpán;
 $this=>$napeti = ustrednaPrefix;
 
 var_dump(ustrednaPrefix);
+
+////////////////////////////////////////////////////////////////////////////////////
+
+public function translate($text){
+
+     $translate_text = $this->ts->translate($text);
+
+     return $translate_text;
+}
+
+public function translate(string $text){
  
+     $translate_text = $this->ts->translate($text);
+
+     return $translate_text;
+}
+
+declare(strict_types=1);
+
+declare(strict_types=1);
+class A {
+}
 
 
+public function translate(string $text) : string {
+ 
+     $translate_text = $this->ts->translate($text);
+
+     return $translate_text;
+}
+
+/**
+ * @param string $text
+ * @return string
+ */
+public function translate(string $text) : string {
+ 
+     $translate_text = $this->ts->translate($text);
+
+     return $translate_text;
+}
+
+/** Vrati prelozeny text
+ * @author Štěpán Boukal
+ * @param string $text
+ * @return string
+ */
+public function translate(string $text) : string {
+ 
+     // preklada text
+     $translate_text = $this->ts->translate($text);
+ 
+    /**
+     * nebo viceradkovy
+     * kometar
+     */
+     return $translate_text;
+}
 
 ?>
