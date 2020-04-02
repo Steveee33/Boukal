624399067864a4ccffe7b5c44b815b7596e1b060
Steveee33 <62248560+Steveee33@users.noreply.github.com>
2020-04-02 10:26:00 +0200

    Update oop.php

--- oop.php
+++ oop.php
@@ -1,31 +1,36 @@
  <?php
 //Štěpán Boukal ZT3
-	}
-	// trida ustredna se dedi do abstraktni tridy ustredny
-	class  ustredna_Boukal extends ustredny_Boukal{
-	   public $Napeti_Boukal = 3;
-	   //nastavi konstanty type
-	   const TYPE = "3";
-	}
-	//trida ustredna dedi interface od abstraktni tridy ustredny
-	interface ustredna_Boukal extends ustredny_Boukal{
+}
+// trida ustredna se dedi do abstraktni tridy ustredny
+class  ustredna_Boukal extends ustredny_Boukal
+{
+	  public $Napeti_Boukal = 3;
+	  //nastavi konstanty type
+	  const TYPE = "3";
+}
+//trida ustredna dedi interface od abstraktni tridy ustredny
+	interface ustredna_Boukal extends ustredny_Boukal
+	{
 	    //vypise nastavene hodnoty
 	    public function _contruct ($Napeti_Boukal);
-	}
-	//trida ktera je abstraktni ustredny a v ni protected $napetí
-	abstract class  ustredny_Boukal{
+}
+//trida ktera je abstraktni ustredny a v ni protected $napetí
+	abstract class  ustredny_Boukal
+	{
 	
 	    protected $napeti_Boukal;
-	    //nastavi napeti
-	    public function setNapeti_Boukal($napeti_Boukal){
+//nastavi napeti
+	    public function setNapeti_Boukal($napeti_Boukal)
+	    {
 	        $this->napeti_Boukal = $napeti_Boukal;
 	    }
-	    //precte napeti
-	    public function getNapeti_Boukal() {
+//precte napeti
+	    public function getNapeti_Boukal() 
+	    {
 	        return $this-> napeti_Boukal;
 	    }
 
-	//pres vardummp nakonec vypiseme hodnotu ustrednoveho napeti a hodnotu type
+//pres vardummp nakonec vypiseme hodnotu ustrednoveho napeti a hodnotu type
 	 var_dump (ustredna_Boukal::TYPE);
 	 var_dump ($Napeti_Boukal);
 	
